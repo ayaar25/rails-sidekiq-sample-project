4ffe5540f42732bb6918180052de7a24a14fe908
ayaar25 <aya.aurora25@gmail.com>
2020-12-13 18:22:34 +0700

    Add figaro configuration
    Co-authored-by: Husnulzaki <husnulzaki.haryadi@gmail.com>

---
+++ config/initializers/figaro.rb
@@ -0,0 +1,5 @@
+Figaro.require_keys('SIDEKIQ_REDIST_HOST',
+                    'POSTGRESS_USER',
+                    'POSTGRESS_PASSWORD',
+                    'POSTGRESS_HOST',
+                    'POSTGRESS_DB')
